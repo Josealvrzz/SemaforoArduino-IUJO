8cb7866d98c7e87e791e3fb41487d665261a9b57
Jose Manuel Castro Lopez <castrojuego88@gmail.com>
2026-08-01 22:03:53 -0400

    feat: definir pines de semáforos y peatones

---
+++ SemaforoArduino.ino
@@ -0,0 +1,13 @@
+// Pines Vías Norte y Sur (Conectados en paralelo)
+const int cocheVerdeNS = 2;
+const int cocheAmarilloNS = 3;
+const int cocheRojoNS = 4;
+const int peatonVerdeNS = 5;
+const int peatonRojoNS = 6;
+
+// Pines Vías Este y Oeste (Conectados en paralelo)
+const int cocheVerdeEO = 7;
+const int cocheAmarilloEO = 8;
+const int cocheRojoEO = 9;
+const int peatonVerdeEO = 10;
+const int peatonRojoEO = 11;
